774d24b10dfe68375ad8129ab784e2e212f91dab
Ihor Hamal <him.igor@gmail.com>
2015-10-18 00:10:40 +0300

    Config sample

---
+++ config/main.sample.php
@@ -0,0 +1,16 @@
+<?php
+
+$settings = array(
+    'db' => array(
+        'type' => 'sqlite',
+        'db' => dirname(__file__) . '/../db/database.sdb',
+        ),
+    'encryption' => array(
+        'prefix' => '',
+        'suffix' => '',
+        ),
+    'mysql' => array(
+        'hostname' => 'localhost',
+        'username' => '',
+        'password' => ''),
+    );
